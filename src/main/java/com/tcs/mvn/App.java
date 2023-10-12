7812c28d511a450975922f3b2db217c375bc01d4
Your Name <you@example.com>
2023-10-12 08:48:24 +0530

    first commit

---
+++ src/main/java/com/tcs/mvn/App.java
@@ -0,0 +1,13 @@
+package com.tcs.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
